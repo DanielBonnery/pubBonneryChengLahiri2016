4f46080f9f382002f6da1f363ba574c6d4522141
DanielBonnery <daniel.bonnery@gmail.com>
2016-05-23 23:20:49 -0400

    a

--- demo/Simultation.R
+++ demo/Simultation.R
@@ -1,5 +1,6 @@
 library(CompositeRegressionEstimation)
 library(pubBonneryChengLahiri2016)
+options(cores=8)
 Totals      <-
   plyr::aaply(
     pubBonneryChengLahiri2016::S2comp,1,function(x){
@@ -9,11 +10,15 @@ Totals      <-
 names(dimnames(Totals))<-c("month","pumlrR")
 crossTotals<-pubBonneryChengLahiri2016::CountsChangePumlrR
 
-
 syntheticcpspops<-syntheticcpsdataset(Totals,crossTotals)
 
 syntheticcpspopsHA<-plyr::laply(syntheticcpspops,list.tablespopAf)
-dimnames(syntheticcpspopsHA)<-
+
+
+####################################################
+# Computation of pop totals
+####################################################
+Populationtotals<-plyr::aaply(syntheticcpspopsHA,1:3,sum)
 gc()
 
 #Draw all probable samples
@@ -22,93 +27,50 @@ gc()
 #Computation of month in sample totals.
 
 #list.tablesA<-list.tablesAf(syntheticcpspopsHA,AllsamplesH)
-misestimates<-125*plyr::maply(expand.grid(i=1:1000,m=1:85,misi=1:8),misH,syntheticcpspopsHA=syntheticcpspopsHA,.progress="text")
+misestimates<-800*plyr::maply(expand.grid(i=1:1000,m=1:85,misi=1:8),misH,syntheticcpspopsHA=syntheticcpspopsHA,.progress="text")
 dimnames(misestimates)<-list(1:1000,dimnames(syntheticcpspopsHA)[c(1,3,4)])
-names(dimnames(misestimates))<-c("i (longitudinal sample)", "m (month)","j (month in sample)","Employment status")
-Hmisc::label(misestimates)<-"Month in sample estimate for longitudinal sample i, month m, rotation group mis j"
+names(dimnames(misestimates))<-c("i","m","j","s","y")
+Hmisc::label(misestimates)<-"Month in sample estimate for longitudinal sample i, month m, rotation group mis j, synthetisation procedure s, employment statys y"
+
+
+
 
 
 #Computation of direct estimator
 #First method:
-Direct<-plyr::aaply(misestimates,c(),mean)
-#second method
+Direct<-plyr::aaply(misestimates,c(1:2,4:5),sum,.progress="text")
 
 
-
-#Computation of month in sample totals
-hrmis=factor(rep(8:1,each=100))
-
-
-
-    what="S2"
-    what="MRR"
-    what="S2"
-    what="S";what="mistotals";what="AK2_papa";what="S2";what="MRR";what="BCL2";what="BCL0";what="BCLratio"
-    what=c("AK2_papa","S2")
-    what=c("MRR","BCL2","BCL0","BCLratio")
-    u=1:nrep
-    u=NULL
-    popnum=1
-    popnum=2
-    bias=""
-    bias="bias"
+# Computation of Sigma          
+Sigmas<-plyr::aaply(misestimates,4,function(x){
+    Sigma=array(var(array(x,
+                            c(dim(x)[1],prod(dim(x)[2:4])))),
+                rep(dim(x)[2:4],2))
+    dimnames(Sigma)<-rep(dimnames(x)[2:4],2)
+    names(dimnames(Sigma))<-paste0(names(dimnames(Sigma)),rep(1:2,each=3))
+    Sigma},.progress="text")
     
-    #   Compute_S2MRAK_rep(what=what,u=u)
-    u=1:nrep;
-    what=c("mistotals","covar","bestak")
-    Compute_S2MRAK_rep(what=what,u=u,popnum=2)
-    Compute_S2MRAK_rep(what=what,u=u,popnum=3)
-    what=c("AK2_papa","S2","MRR","BCL2","BCL0","BCLratio")
-    Compute_S2MRAK_rep(what=what,u=u,popnum=2)
-    Compute_S2MRAK_rep(what=what,u=u,popnum=3)
-    bias="bias"
-    what=c("mistotals","covar","bestak")
-    Compute_S2MRAK_rep(what=what,bias="bias",u=u,popnum=2)
-    Compute_S2MRAK_rep(what=what,bias="bias",u=u,popnum=3)
-    what=c("AK2_papa","S2","MRR")#,"BCL0","BCLratio")
-    Compute_S2MRAK_rep(what=what,bias="bias",u=u,popnum=2)
-    Compute_S2MRAK_rep(what=what,bias="bias",u=u,popnum=3)
-    what=c("BCL2")#,"BCL0","BCLratio")
-    Compute_S2MRAK_rep(what=what,bias="bias",u=u,popnum=2)
-    Compute_S2MRAK_rep(what=what,bias="bias",u=u,popnum=3)
-    
-    
-    what=c("coeffYF")#,"BCL0","BCLratio")
-    Compute_S2MRAK_rep(what=what,u=u,popnum=1)
-    Compute_S2MRAK_rep(what=what,u=u,popnum=2)
-    Compute_S2MRAK_rep(what=what,u=u,popnum=3)
-    
-    what=c("YF","S2lin")#,"BCL0","BCLratio")
-    Compute_S2MRAK_rep(what=what,u=u,popnum=1)
-    Compute_S2MRAK_rep(what=what,u=u,popnum=2)
-    Compute_S2MRAK_rep(what=what,u=u,popnum=3)
-    Compute_S2MRAK_rep(what=what,bias="bias",u=u,popnum=1)
-    Compute_S2MRAK_rep(what=what,bias="bias",u=u,popnum=2)
-    Compute_S2MRAK_rep(what=what,bias="bias",u=u,popnum=3)
-    #Compute_S2MRAK_rep(what=c("AK_papa","RA","S2"),u=NULL)
-    rm(list.tablespop)
-  }
-  
-  Compute_S2(list.y=if(web){c("pumlrR")}else{list.y=c("pumlr","pehspnon","pesex")})
-  Compute_AK(list.tables=list.tables,
-             list.y=if(web){c("pumlrR")}else{list.y=c("pumlr","pehspnon","pesex")},
-             id=if(web){c("hrlongid","pulineno")}else{NULL},
-             groupvar=if(web){NULL}else{"hrmis"})
-  #Compute_Ratio(list.tables,pcserv)
-  computemistotalssimple(list.tables)
-  Compute_AK2(list.tables=list.tables)
-  
-  #Compute_MRP()
-  #Compute_MRPR()
-  #Compute_BCL()
-  Compute_BCL2(list.tables)
-  Compute_MR()
-  Compute_MRR(list.tables,pcserv)
-  #rm(list.tables)
-  #load(paste0(tablesfolder,"/list.tablesregroup",pcserv,".Rdata"))
-  #Compute_MRRH()
-  #rm(list.tablesregroup)
-}}}
-#Analysis
-if(steps[6]){if(PC){
-  analysisalpha(TRUE)}}
+# Computation of coefficients for Best linear estimates (Yansaneh fuller)
+
+CoeffYF<-function (Sigma) 
+{
+  nmonth<-dim(Sigma)[1]
+  Sigg <- array(aperm(Sigma, c(3, 2, 1, 6, 5, 4)), rep(nmonth * 8 * 3, 2))
+  XX <- t(matrix(rep(diag(3), 8), 3, 24))
+  X <- as.matrix(Matrix::.bdiag(lapply(1:nmonth, function(i) {
+    XX
+  })))
+  Xplus <- t(X)/8
+  M <- diag(nmonth * 8 * 3) - X %*% Xplus
+  gi <- MASS::ginv(M %*% Sigg %*% M)
+  W <- Xplus %*% (diag(nmonth * 8 * 3) - M) %*% (diag(nmonth * 
+                                                        8 * 3) - Sigg %*% gi)
+  return(W)
+}
+
+coeffYF<-plyr::aaply(Sigmas,1,function(Sigma){CoeffYF(Sigma)},.progress="text")
+
+
+# Computation of coefficients Best AK estimator
+
+# Computation of coefficients Best AK estimator
