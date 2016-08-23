1bb8fd3f3ed8d99367b6cfdf0cf8c3dc978b68ae
DanielBonnery <daniel.bonnery@gmail.com>
2016-08-23 15:57:08 -0400

    a

--- demo/ComputeestimatesonCPSwebdata.R
+++ demo/ComputeestimatesonCPSwebdata.R
@@ -9,6 +9,8 @@ library('filehash')
 library(dataCPS)
 library(CompositeRegressionEstimation)
 library(plyr)
+library(ggplot2)
+library(scales)
 
 #1. List of tables
 allmonths <- format(seq(as.Date("20050101", "%Y%m%d"),
@@ -46,14 +48,14 @@ Estimates_AK_web<-AK3(dfest = 8*Estimates_Mis_web)
 dimnames(Estimates_AK_web)[[2]]<-allmonths
 #AKestimatesweb2<-AK2(dfest = mistotalsweb,ak = do.call(c,plyr::alply((0:10)/10,1,function(a){plyr::alply((0:10)/10,1, function(k){list(a=a,k=k)})})))
 Estimates_AK_e_web<-AK(list.tables = list.tablesweb,w = "pwsswgt",id = c("hrhhid","pulineno"),groupvar = "hrmis",
-                       groups_1 =c("2","3","4","5","7","8"),groups_0=paste0(c(1:3,5:7),""),list.y="employed",A=ACPSemployed(),K=KCPSemployed())
+                       groups_1 =c("1","2","3","5","6","7"),groups_0=paste0(c(2:4,6:8),""),list.y="employed",A=ACPSemployed(),K=KCPSemployed())
 Estimates_AK_u_web<-AK(list.tables = list.tablesweb,w = "pwsswgt",id = c("hrhhid","pulineno"),groupvar = "hrmis",
-                       groups_1 =c("2","3","4","5","7","8"),groups_0=paste0(c(1:3,5:7),""),list.y="unemployed",A=ACPSunemployed(),K=KCPSunemployed())
+                       groups_1 =c("1","2","3","5","6","7"),groups_0=paste0(c(2:4,6:8),""),list.y="unemployed",A=ACPSunemployed(),K=KCPSunemployed())
 # Estimation using composite weights
 Estimates_AK4_web<-WS(list.tablesweb,list.y = "pemlrR",weight ="pwcmpwgt" )
 
 #Check we get the same estimates with the two different methods:
-cbind(Estimates_AK_web[1,,1:2],Estimates_AK_u_web,Estimates_AK_e_web)
+cbind(Estimates_AK_web[1,,1:2],Estimates_AK_u_web,Estimates_AK_e_web, Estimates_Direct_web,Estimates_AK4_web)
 
 all(abs(Estimates_AK_web[1,,1:2]-cbind(Estimates_AK_u_web,Estimates_AK_e_web))<1.e-4)
 #5.Computation of Modified regression estimates, computation of X2 totals.
@@ -70,11 +72,12 @@ Estimates_MR_web<-MR(list.tables=list.tablesweb,
                      list.x2=c("pesex"),
                      list.dft.x2=list.dft.x2)$dfEst
 Estimates_MR_webA<-plyr::laply(Estimates_MR_web,function(x){x})
-dimnames(Estimates_MR_webA)[[1]]<-paste0("MR",names(Estimates_MR_webA))
+dimnames(Estimates_MR_webA)[[1]]<-paste0("MR",names(Estimates_MR_web))
 dimnames(Estimates_MR_webA)[[2]]<-allmonths
 
 #6. Put all estimates in the same dataframe
-Estimates_all_web<-abind::abind(Estimates_MR_webA,Estimates_AK_web,Direct=array(Estimates_Direct_web,c(1,dim(Estimates_Direct_web))),along=1)
+Estimates_all_web<-abind::abind(Estimates_MR_webA,Estimates_AK_web,Direct=array(Estimates_Direct_web,c(1,dim(Estimates_Direct_web)))
+                                ,AK=array(Estimates_AK4_web,c(1,dim(Estimates_AK4_web))),along=1)
 
 names(dimnames(Estimates_all_web))<-c("Estimator",names(dimnames(Estimates_Direct_web)))
 
@@ -88,20 +91,17 @@ Estimates_all_change_rel_webD<-reshape2::melt(Estimates_all_change_rel_web,varna
 Estimates_all_change_rel_webD$Month<-as.Date(paste0(Estimates_all_change_rel_webD$Month,"01"), "%Y%m%d")
 
 #7. Create figure 2 of "Multivariate Composite Estimation with An Application to the U.S. Labor Force Statistics"
-library(ggplot2)
-library(scales)
 figure2.a<-ggplot(data=Estimates_all_rel_webD[Estimates_all_rel_webD$Variable=="r"&
-                                 is.element(Estimates_all_rel_webD$Estimator,c("MR0","MR1","MR0.75","AK3-0.3,0.4,0.4,0.7")),],
+                                 is.element(Estimates_all_rel_webD$Estimator,c("MR0","MR1","MR0.75","AK")),],
        aes(x=Month,y=value)) + geom_line(aes(color=Estimator))+ scale_y_continuous("Unemployment rate") +
   scale_x_date(date_breaks = "1 year", date_minor_breaks = "1 month", date_labels="%Y")+xlab("Time") 
 
 
 figure2.b<-ggplot(data=Estimates_all_change_rel_webD[Estimates_all_change_rel_webD$Variable=="r"&
-                                                is.element(Estimates_all_change_rel_webD$Estimator,c("MR0","MR1","MR0.75","AK3-0.3,0.4,0.4,0.7")),],
+                                                is.element(Estimates_all_change_rel_webD$Estimator,c("MR0","MR1","MR0.75","AK")),],
                   aes(x=Month,y=value)) + geom_line(aes(color=Estimator))+ scale_y_continuous("Unemployment rate") +
   scale_x_date(date_breaks = "1 year", date_minor_breaks = "1 month", date_labels="%Y")+xlab("Time") 
 
 
-#setwd("~Dropbox/CensusBAE/R/Computation of FR/packages/pubBonneryChengLahiri2016")
-#save(figure2.a,figure2.b,file=data/tablesandfigures.rda)
+#save(figure2.a,figure2.b,file="data/tablesandfigures.rda")
 
